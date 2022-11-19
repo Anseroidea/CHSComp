bb4e43ec1c3b97901281f4bacbea7860b6a34e0e
Croissant676 <croissant676@outlook.com>
2022-11-19 16:10:54 -0600

    finished competition :) 5th place apps

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Clements High School Computer Science Competition Code
+
+11/19/2022
+
+Uploaded the code for study.
+5th place.
+
+Problems are marked by who solved them and their status.
--- src/marble.java
+++ src/marble.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 // incomplete
-// author: aidan
+// author: aayush
 public class marble {
 
     public static void main(String[] args) throws IOException {
